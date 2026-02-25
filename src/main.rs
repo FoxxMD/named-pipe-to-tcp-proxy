22aa6559f70612dff4a615e2f2dd469fa61cdd93
FoxxMD <FoxxMD@users.noreply.github.com>
2026-02-24 21:12:44 -0500

    its working, but badly

--- src/main.rs
+++ src/main.rs
@@ -68,24 +68,26 @@ async fn main() {
     use named_pipe_server::NamedPipeServerManager;
     info!("Using pipe: {}", &args.pipe);
     let mut server = NamedPipeServerManager::new(args.pipe);
-    //loop {
-        info!("test");
-        // A client has connected to the server
-        let mut pipe_stream = server.accept().await.unwrap();
+
+            
         // Each Pipe Stream should have its own TCP Connection
         // This makes it easier to logically read and write code
         //let mut tcp_stream = TcpStream::connect(&args.tcp).await.unwrap();
         info!("Binding {}", &args.tcp);
         let tcp_port = TcpListener::bind(&args.tcp).await.unwrap();
-
         let mut atcp_stream = TcpListener::accept(&tcp_port).await.unwrap();
+        let mut pipe_stream = server.accept().await.unwrap();
+    loop {
+        info!("test");
+        // A client has connected to the server
+        
         // Spawn a new Future to achieve concurrency
-        tokio::spawn(async move {
+        //tokio::spawn(async move {
             // Each connection should have its own Buffer
             let mut buf = vec![0; 1024 * 8];
             // Note that even if this returns an error we do not really care
             // The only reason the let exists here is so we do not get warnings
             let _ = splice(&mut pipe_stream, &mut atcp_stream.0, &mut buf).await;
-        });
-   // }
+        //});
+    }
 }
